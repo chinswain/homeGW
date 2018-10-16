8df4d7c21e26357424aa93b239b4c7962cf40e70
chinswain <madlan@yahoo.com>
2018-10-16 23:30:59 +0100

    Update serial_Print.ino

--- examples/serial_Print/serial_Print.ino
+++ examples/serial_Print/serial_Print.ino
@@ -1,8 +1,10 @@
 #include <homeGW.h>
 #include <digoo.h>
 
-HomeGW gw(1);
+HomeGW gw(3);
 digoo station1;
+digoo station2;
+digoo station3;
 
 uint64_t prev_p = 0;
 
@@ -17,6 +19,8 @@ void setup() {
   digitalWrite(RF_RECEIVER_PIN, LOW);
   gw.setup(RF_RECEIVER_PIN);
   gw.registerPlugin(&station1);
+  gw.registerPlugin(&station2);
+  gw.registerPlugin(&station3);
 }
 
 void loop() {
@@ -39,5 +43,37 @@ void loop() {
       }
       prev_p = p;
     }
+  
+  if (station2.available())
+    if ((p = station2.getPacket())) {
+      if (p == prev_p) {
 
+        Serial.print("ID: ");  Serial.print(station2.getId(p));
+        Serial.print("\tChannel: ");  Serial.print(station2.getChannel(p));
+        Serial.print("\tBattery: ");  Serial.print(station2.getBattery(p));
+        Serial.print("\tTemperature: ");  Serial.print(station2.getTemperature(p));
+        Serial.print("\tHumidity: ");  Serial.print(station2.getHumidity(p));
+        Serial.print("\tString: ");  Serial.println(station2.getString(p));
+
+        p = 0;
+      }
+      prev_p = p;
+    }
+
+  if (station3.available())
+    if ((p = station3.getPacket())) {
+      if (p == prev_p) {
+
+        Serial.print("ID: ");  Serial.print(station3.getId(p));
+        Serial.print("\tChannel: ");  Serial.print(station3.getChannel(p));
+        Serial.print("\tBattery: ");  Serial.print(station3.getBattery(p));
+        Serial.print("\tTemperature: ");  Serial.print(station3.getTemperature(p));
+        Serial.print("\tHumidity: ");  Serial.print(station3.getHumidity(p));
+        Serial.print("\tString: ");  Serial.println(station3.getString(p));
+
+        p = 0;
+      }
+      prev_p = p;
+    }
+  
 }
